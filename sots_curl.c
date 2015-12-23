fd58d8891995422743c81a1675d86c32ff733825
Rob Allen <rob@akrabat.com>
2015-12-23 18:18:27 +0000

    Add curl_easy_setopt_func

--- sots_curl.c
+++ sots_curl.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <curl/curl.h>
+#include "sots_curl.h"
 
 
 CURLcode curl_easy_setopt_string(CURL *curl, CURLoption option, const char *param) {
@@ -8,4 +9,9 @@ CURLcode curl_easy_setopt_string(CURL *curl, CURLoption option, const char *para
 
 CURLcode curl_easy_setopt_bool(CURL *curl, CURLoption option, bool param) {
     return curl_easy_setopt(curl, option, param);
+}
+
+CURLcode curl_easy_setopt_func(CURL *handle, CURLoption option, curl_func param)
+{
+    return curl_easy_setopt(handle, option, param);
 }
--- sots_curl.h
+++ sots_curl.h
@@ -1,10 +1,12 @@
-#ifndef __RKA_CURL_H
-#define __RKA_CURL_H
+#ifndef __SOTS_CURL_H
+#define __SOTS_CURL_H
 
 #include <stdbool.h>
 
+typedef size_t (*curl_func)(void * ptr, size_t size, size_t num, void * ud);
 
 CURLcode curl_easy_setopt_string(CURL *curl, CURLoption option, const char *param);
 CURLcode curl_easy_setopt_bool(CURL *curl, CURLoption option, bool param);
+CURLcode curl_easy_setopt_func(CURL *handle, CURLoption option, curl_func param);
 
 #endif
